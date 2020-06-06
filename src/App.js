38be648a1dbb44cd28ad60e3f4248d4513948d31
Khristian Brooks <kh.brooks02@gmail.com>
2020-06-06 17:47:36 -0400

    add react-router to project

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation';
 
 const App = () => {
 	return (
-		<div className='App'>
-			<Navigation />
+		<Router>
+			<div>
+				<Navigation />
 
+				{/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+				<Switch>
+					<Route path='/about'>
+						<About />
+					</Route>
+					<Route path='/users'>
+						<Users />
+					</Route>
+					<Route path='/'>
+						<Home />
+					</Route>
+				</Switch>
+			</div>
+		</Router>
+	);
+};
+
+function About() {
+	return <h2>About</h2>;
+}
+
+function Users() {
+	return <h2>Users</h2>;
+}
+
+const Home = () => {
+	return (
+		<div className='App'>
 			<header className='App-header'>
 				<img src={logo} className='App-logo' alt='logo' />
 				<p>
