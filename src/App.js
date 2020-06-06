440ac625f7e1153ef4cb0d43a0783cd4c5224d23
Khristian Brooks <kh.brooks02@gmail.com>
2020-06-06 18:28:25 -0400

    add about and home pagea

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-import logo from './logo.svg';
-import './App.css';
 import Navigation from './components/Navigation';
+import Home from './pages/Home';
+import About from './pages/About';
 
 const App = () => {
 	return (
@@ -29,33 +29,8 @@ const App = () => {
 	);
 };
 
-function About() {
-	return <h2>About</h2>;
-}
-
 function Users() {
 	return <h2>Users</h2>;
 }
-
-const Home = () => {
-	return (
-		<div className='App'>
-			<header className='App-header'>
-				<img src={logo} className='App-logo' alt='logo' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
-	);
-};
 
 export default App;
---
+++ src/pages/About.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<div>
+			<p>
+				Nateinyostate is a passion project that got started at a young age of
+				traveling and telling the world about it. It all started with a bike
+				trip that I did with a high friend and cycling enthusiast, Will. We did
+				an impromptu trip from The Bronx, New York all the way to Washington DC
+				in 2009. But as I sit here writing this it goes deeper than that. It
+				actually goes as far back to the 90s when I got my first bicycle living
+				in the South Bronx. My parents always wanted to keep an eye on me so my
+				range of motion was very limited back in the day. When I got that bike
+				however I was able to go a little further with my parents permission.
+				This was almost “liberating” as it opened the world around me.{' '}
+			</p>
+			<p>
+				As I got more involved in cycling, Will and I started to cycle from The
+				Bronx all the way down to Chinatown in Manhattan. This was just for fun
+				at first, but we made it into a thing to bike to Chinatown whenever we
+				were hungry because the Chinese food there was genuine compared to our
+				neighborhoods. New York couldn't contain our appetite to explore new
+				places to cycle and we biked into New Jersey. After we conquered another
+				state, personally I wanted to go further; pushing my limits. Will
+				jokingly suggested we go to D.C and I said “Yeah, let's do it!” and
+				within a few days we were on our way.{' '}
+			</p>
+			<p>
+				After a health scare in 2018, I realized how short life is and made a
+				balance on eating right as well as sharing unique places to eat. I took
+				traveling and exploring more seriously. I want to see as much of the
+				world as I can and I set out on a journey to enjoy the things I love.
+				Food, Travel, and Tech. I hope to continue having me aboard on this
+				journey.
+			</p>
+		</div>
+	);
+};
+
+export default About;
